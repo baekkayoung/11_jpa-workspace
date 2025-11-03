d63c794ab1edae48717e73a7f46caee78257e16b
baek <baekkayoung@naver.com>
2025-11-03 17:48:54 +0900

    11/3

--- jpql/src/main/java/hellojpa/JpaMain.java
+++ jpql/src/main/java/hellojpa/JpaMain.java
@@ -42,56 +42,82 @@ public class JpaMain {
             member2.setMemberType(MemberType.USER);
             em.persist(member2);*/
 
-            Product product1 = new Product();
-            product1.setName("Apple");
-            product1.setPrice(100);
-            product1.setStockAmount(3);
-            em.persist(product1);
+//            Product product1 = new Product();
+//            product1.setName("Apple");
+//            product1.setPrice(100);
+//            product1.setStockAmount(3);
+//            em.persist(product1);
+//
+//            Product product2 = new Product();
+//            product2.setName("melon");
+//            product2.setPrice(2000);
+//            product2.setStockAmount(3);
+//            em.persist(product2);
+//
+//            Product product3 = new Product();
+//            product3.setName("테스트 상품");
+//            product3.setPrice(6000);
+//            product3.setStockAmount(3);
+//            em.persist(product3);
+//
+//            Address address1 = new Address("서울", "테헤란로", "10000");
+//            Address address2 = new Address("인천", "선학로", "21910");
+//
+//            Order order1 = new Order();
+//            order1.setAddress(address1);
+//            order1.setOrderAmount(1);
+//            order1.setProduct(product1);
+//            em.persist(order1);
+//
+//            Order order2 = new Order();
+//            order2.setAddress(address2);
+//            order2.setOrderAmount(2);
+//            order2.setProduct(product2);
+//            em.persist(order2);
+//
+//            Order order3 = new Order();
+//            order3.setAddress(address2);
+//            order3.setOrderAmount(3);
+//            order3.setProduct(product3);
+//            em.persist(order3);
 
-            Product product2 = new Product();
-            product2.setName("melon");
-            product2.setPrice(2000);
-            product2.setStockAmount(3);
-            em.persist(product2);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Product product3 = new Product();
-            product3.setName("테스트 상품");
-            product3.setPrice(6000);
-            product3.setStockAmount(3);
-            em.persist(product3);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
-            Address address1 = new Address("서울", "테헤란로", "10000");
-            Address address2 = new Address("인천", "선학로", "21910");
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
 
-            Order order1 = new Order();
-            order1.setAddress(address1);
-            order1.setOrderAmount(1);
-            order1.setProduct(product1);
-            em.persist(order1);
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
 
-            Order order2 = new Order();
-            order2.setAddress(address2);
-            order2.setOrderAmount(2);
-            order2.setProduct(product2);
-            em.persist(order2);
-
-            Order order3 = new Order();
-            order3.setAddress(address2);
-            order3.setOrderAmount(3);
-            order3.setProduct(product3);
-            em.persist(order3);
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select new hellojpa.dto.OrderProductDTO(o.orderAmount, name) " +
-                    "from Order o join o.product p";
+            String query = "update Member m set m.age = 20";
+            int resultCount = em.createQuery(query).executeUpdate(); // excuteupdate는 행수를 반환 -> 변수 int
 
-            List<OrderProductDTO> resultList = em.createQuery(query, OrderProductDTO.class).getResultList();
+            em.clear(); // 영속성 컨테스트 초기화.
+            Member findMember = em.find(Member.class, member1.getId());// db에 잇는것 가지고 와서 영속 상태로 만들어 줌
+            
+            System.out.println("resultCount : "+ resultCount);
 
-            for (OrderProductDTO orderProductDTO : resultList) {
-                System.out.println("주문의 금액 : " + orderProductDTO.getOrderAmount() + " 주문한 상품 이름 : "+ orderProductDTO.getProductName());
-            }
+            System.out.println("member1.age  : " + findMember.getAge());
+            // 영속성 컨테스트 안에서 가지고 오는 것 . -> 0살
+            // 만약 clear로 비워주면 -> 영속성 컨테스트에 아무것도 없음 -> db로 가서 가지고 옴!
 
 
 //            String query = "select m from Member m where m.memberType = hellojpa.jpql.MemberType.ADMIN";
@@ -100,8 +126,7 @@ public class JpaMain {
 //            for (Member member1 : resultList) {
 //                System.out.println("Admin인 회원 : " + member1);
 //            }
-
-
+            tx.commit();
 
         } catch (Exception e) {
             tx.rollback();
